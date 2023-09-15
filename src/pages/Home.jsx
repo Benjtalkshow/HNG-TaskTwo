4a4c2a508ff3e8ccd37d028af5643ae585fd377e
Benjtalkshow <chinedubenj@gmail.com>
2023-09-15 16:25:25 -0700

    adjusted the responsiveness of my home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,100 +64,145 @@ const Home = () => {
     }, 1000);
   }, []);
 
+  const [randomMovie, setRandomMovie] = useState(null);
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const apiKey = "7f23fe1838341209abe18781249c152f";
+        const language = "en-US";
+        const sortBy = "popularity.desc";
+        const page = 10;
+
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=${language}&sort_by=${sortBy}&page=${page}`;
+
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        selectRandomMovie(data.results);
+
+        const intervalId = setInterval(() => {
+          selectRandomMovie(data.results);
+        }, 5000 * 10);
+
+        return () => clearInterval(intervalId);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  // random movie posters, ratings and description
+  const selectRandomMovie = (movieList) => {
+    const randomIndex = Math.floor(Math.random() * movieList.length);
+    const randomMovie = movieList[randomIndex];
+    setRandomMovie(randomMovie);
+  };
+
   return (
     <div className="flex flex-col bg-[#F4F5F7] min-h-screen">
-      <header className="flex flex-col gap-[4rem] px-2 md:px-8 py-2">
-        <nav className="flex items-center justify-between text-white relative">
-          <div className="logo flex items-center md:gap-2 md:basis-1/4">
-            <img src={logo} alt="Movie Box Logo" />
+      {randomMovie && (
+        <header
+          className="flex flex-col gap-[4rem] px-2 md:px-8 py-2 relative"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${randomMovie.poster_path})`,
+          }}
+        >
+          <div className="absolute inset-0 bg-black opacity-40"></div>
+          <nav className="flex items-center justify-between md:justify-around text-white relative">
+            <div className="logo flex items-center md:gap-2 md:basis-1/4">
+              <img src={logo} alt="Movie Box Logo" />
 
-            <span className="logo_text font-semibold md:text-lg ms-2">
-              MovieBox
-            </span>
-          </div>
-          <div className="group ms-4 md:basis-1/2">
-            <input
-              placeholder="Search for a movie..."
-              className="input"
-              value={searchedMovie}
-              onChange={(event) => {
-                setSearchedMovie(event.target.value.toLowerCase());
-              }}
-            />
-            <HiSearch className="icon cursor-pointer " onClick={searchMovie} />
-          </div>
-          <div className="hidden md:flex items-center justify-end gap-2 md:basis-1/4">
-            <span className="font-semibold">Sign in</span>
-            <div className="bg-rose-700 w-[36px] h-[36px] rounded-full grid place-content-center">
-              <HiMenuAlt4 />
+              <span className="logo_text font-semibold md:text-lg ms-2">
+                MovieBox
+              </span>
             </div>
-          </div>
-          {isError ? (
-            <div className="warning absolute top-0 right-0 animate__animated animate__bounceIn">
-              <div className="warning__icon">
-                <svg
-                  fill="none"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="m13 14h-2v-5h2zm0 4h-2v-2h2zm-12 3h22l-11-19z"
-                    fill="#393a37"
-                  ></path>
-                </svg>
-              </div>
-              <div className="warning__title">
-                You entered an empty value, try again!
-              </div>
-              <div className="warning__close" onClick={errorCheck}>
-                <svg
-                  height="20"
-                  viewBox="0 0 20 20"
-                  width="20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z"
-                    fill="#393a37"
-                  ></path>
-                </svg>
+            <div className="searchbar ms-4 md:basis-1/2">
+              <input
+                placeholder="Search for a movie..."
+                className="input"
+                value={searchedMovie}
+                onChange={(event) => {
+                  setSearchedMovie(event.target.value.toLowerCase());
+                }}
+              />
+              <HiSearch
+                className="icon cursor-pointer "
+                onClick={searchMovie}
+              />
+            </div>
+            <div className="flex items-center justify-end gap-2 md:basis-1/4">
+              <span className="font-semibold">Sign in</span>
+              <div className="bg-rose-700 w-[36px] h-[36px] rounded-full grid place-content-center">
+                <HiMenuAlt4 />
               </div>
             </div>
-          ) : null}
-        </nav>
-        <div className="text-white md:w-[40%] flex flex-col gap-3">
-          <h1 className="text-lg md:text-xl font-semibold">
-            John Wick 3 : <br /> Parabellum
-          </h1>
-          <div className="flex gap-x-3">
-            <div className="flex gap-1 items-center">
-              <img src={imdb} className="w-[50px]" loading="lazy" />
-              <span className="text-[12px]">74 / 100</span>
+            {isError ? (
+              <div className="warning absolute top-0 right-0 animate__animated animate__bounceIn">
+                <div className="warning__icon">
+                  <svg
+                    fill="none"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m13 14h-2v-5h2zm0 4h-2v-2h2zm-12 3h22l-11-19z"
+                      fill="#393a37"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="warning__title">
+                  You entered an empty value, try again!
+                </div>
+                <div className="warning__close" onClick={errorCheck}>
+                  <svg
+                    height="20"
+                    viewBox="0 0 20 20"
+                    width="20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z"
+                      fill="#393a37"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+            ) : null}
+          </nav>
+          <div className="text-white md:w-[40%] flex flex-col mt-10 gap-3 z-10">
+            <h1 className="text-lg md:text-xl font-semibold">
+              {randomMovie.title}
+            </h1>
+            <div className="flex gap-x-3">
+              <div className="flex gap-1 items-center">
+                <img src={imdb} className="w-[50px]" loading="lazy" />
+                <span className="text-sm sm:text-md">{randomMovie.popularity}</span>
+              </div>
+              <div className="flex gap-1 items-center">
+                <img src={tomato} className="w-[25px]" loading="lazy" />
+                <span className="text-sm">{randomMovie.vote_average}%</span>
+              </div>
             </div>
-            <div className="flex gap-1 items-center">
-              <img src={tomato} className="w-[25px]" loading="lazy" />
-              <span className="text-[12px]">74%</span>
-            </div>
+            <p className="text-md md:text-[20px]">{randomMovie.overview}</p>
+            <button className="watchTrailerBtn shadow-md  text-white md:text-md flex w-full sm:w-[40%] p-2 gap-x-3 justify-center items-center my-5 bg-custom-dark-Red-Color rounded-lg">
+              <img
+                className="watchTrailer w-5 h-5"
+                src={playBtn}
+                alt="watch trailer"
+              />
+              WATCH TRAILER
+            </button>
           </div>
-          <p className="text-[12px] md:text-md">
-            Super-assassin John Wick returns with a $14 million price tag on his
-            head and an army of bounty-hunting killers on his trail. After
-            killing a member of the shadowy international assassin's guild, the
-            High Table, John Wick is excommunicado, but the world's most
-            ruthless hit men and women await his every turn.
-          </p>
-          <button className="watchTrailerBtn shadow-md text-white flex p-4 gap-3 w-[40%] justify-center  items-center my-5 bg-[#be123c] rounded-lg">
-            <img
-              className="watchTrailer w-5 h-5"
-              src={playBtn}
-              alt="watch trailer"
-            />
-            WATCH TRAILER
-          </button>
-        </div>
-      </header>
+        </header>
+      )}
+      ;
       <Movies movieList={movieList} />
       <Footer />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-dark-Red-Color': '#be123c',
+        'custom-black': '#000000',
+        'custom-white': '#ffffff',
+        'custom-bg-OffWhite': '#faf5f7',
+        'custom-grey': '#666666',
+        'custom-dark-Grey': '#333333',
+        'custom-charcoal-Grey': '#404040',
+        'custome-vibrant-Red': '#BE123C',
+      },
+      screens: {
+        'xs': '320px',
+        'sm': '480px',
+        'md': '768px',
+        'lg': '976px',
+        'xl': '1440px',
+      },
+    },
     fontFamily: {
-      DM: ["DM Sans", "sans-serif"],
+      DM: ["Poppins", "sans-serif"],
     },
     fontSize: {
       sm: "14px",
